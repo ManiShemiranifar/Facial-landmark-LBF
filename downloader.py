dd00aa404052c20692c3c17608e002f70216d7c6
ManiShemiranifar <59559426+ManiShemiranifar@users.noreply.github.com>
2021-01-03 10:23:27 -0800

    download LBF & haarcascade

--- downloader.py
+++ downloader.py
@@ -3,7 +3,8 @@ import os
 
 haarcascade_URL = "https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_alt2.xml"
 haarcascade = "haarcascade_frontalface_alt2.xml"
-
+lbfmodel_url = "https://github.com/kurnianggoro/GSOC2017/raw/master/data/lbfmodel.yaml"
+lbfmodel = "lbfmodel.yaml"
 
 def download_haarcascade():
     if haarcascade not in os.listdir(os.curdir):
@@ -21,3 +22,16 @@ def download_haarcascade():
 
 def haarcascade_path():
     return haarcascade
+
+def download_lbf():
+    if lbfmodel not in os.listdir(os.curdir):
+        try:
+            print("Downloading ...")
+            urlreq.urlretrieve(haarcascade_URL, haarcascade)
+            print("Download complete :)")
+        except:
+            print("Download failed :(, please try again")
+
+
+    else:
+        print("You already have LBFmode file")
